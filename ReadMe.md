c62f9f21fc4770add88c388655736dfa0ee32ebf
Rar Zar <yarzarminkhant2003@gmail.com>
2023-07-15 13:55:48 +0630

    New-Setup-Changes

--- ReadMe.md
+++
@@ -1,2 +0,0 @@
-# Main Site - https://weblearnbd.net/tphtml/ebazer/site-settings.html
-# Main Css - https://weblearnbd.net/tphtml/ebazer/assets/css/main.css
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                'sans': ["main-font"]
+            },
+            colors: {
+                textBody: '#000000',
+                primary: '#0092EF',
+            },
+        },
+    },
+    plugins: [],
 }
