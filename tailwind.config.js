841cce8a117827eb8a8354e2fa79db9a3f100ba7
Rar Zar <yarzarminkhant2003@gmail.com>
2023-07-15 15:32:33 +0630

    for-index

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,19 @@ module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
         extend: {
+            fontSize: {
+                tiny: '12px',
+                base: '14px',
+            },
             fontFamily: {
                 'sans': ["main-font"]
             },
             colors: {
                 textBody: '#000000',
-                primary: '#0092EF',
+                danger: 'rgb(241 65 108)',
+                themeLight: 'rgb(225 240 255)',
+                theme: 'rgb(9 137 255)',
+                success: 'rgb(80 205 137)'
             },
         },
     },
