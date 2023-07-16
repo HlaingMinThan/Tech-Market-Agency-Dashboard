3285b778022edd9fc97240c52ae9da2b84b696a7
Rar Zar <yarzarminkhant2003@gmail.com>
2023-07-17 02:58:26 +0630

    php_structure

---
+++ src/common/footer.php
@@ -0,0 +1,15 @@
+    </div>
+</div>
+
+<script src="../../assets/js/alpine.js"></script>
+    <script src="../../assets/js/perfect-scrollbar.js"></script>
+    <script src="../../assets/js/choices.js"></script>
+    <script src="../../assets/js/chart.js"></script>
+    <script src="../../assets/js/apexchart.js"></script>
+    <script src="../../assets/js/quill.js"></script>
+    <script src="../../assets/js/rangeslider.min.js"></script>
+    <script src="../../assets/js/main.js"></script>
+
+</body>
+
+</html>
